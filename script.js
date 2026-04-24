210d5008cf4d9ef00f0f3ad56552ad287e854ad7
mb-cent <milos.buchberger@centenaryuniversity.edu>
2026-04-24 19:39:13 -0400

    Completed steps 1. and 2.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
 
-
+let favFoods = ["Sushi", "Black risotto", "Escargot", "Poke", "Calimari", "Takoyaki"]
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
 
-
+for (let i = 0; i < 6; i++) {
+  console.log("One of my favorite foods is "+ favFoods[i] +".");}
 
 
 
